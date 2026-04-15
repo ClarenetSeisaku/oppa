33ce075ea6820fc6be5c6e2a1acde0c0103114f5
*sGM07ELRdr <sugita@clarenet.co.jp>
2026-04-15 18:14:18 +0900

    セミナーページ波修正

--- single-seminar.php
+++ single-seminar.php
@@ -315,14 +315,11 @@ $breadcrumb_term = (!is_wp_error($breadcrumb_terms) && !empty($breadcrumb_terms)
             </div>
         </div>
 
-        <div class="middle_mv__wave-sway" style="position: relative;">
-            <div class="middle_mv__wave-move">
-                <svg class="middle_mv__wave" viewBox="0 0 1440 150" preserveAspectRatio="none">
-                    <path d="M 0,60 Q 360,100 720,60 T 1440,60 L 1440,150 L 0,150 Z"></path>
-                </svg>
-                <svg class="middle_mv__wave" viewBox="0 0 1440 150" preserveAspectRatio="none">
-                    <path d="M 0,60 Q 360,100 720,60 T 1440,60 L 1440,150 L 0,150 Z"></path>
-                </svg>
+        <div class="middle_mv__decoration">
+            <!-- 波 -->
+            <div class="middle_mv__wave-sway">
+                <div class="middle_mv__wave-move">
+                </div>
             </div>
         </div>
 
